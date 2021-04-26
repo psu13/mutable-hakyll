257d1c63171026d66e8069ae22f53b9853b6fc65
psu <pete.su@gmail.com>
2021-04-25 20:48:28 -0400

    katex

--- site.hs
+++ site.hs
@@ -3,6 +3,9 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 
+import qualified Text.Pandoc.Options as Options
+import qualified Text.Pandoc.Extensions as Extensions
+import qualified Data.Set as Set
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -23,10 +26,7 @@ main = hakyll $ do
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $ postCompiler
 
     create ["archive.html"] $ do
         route idRoute
@@ -64,3 +64,28 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+-- options for katex math
+-- this generates <span> tags for the math. you need to put the right script into
+-- the templates for this to work out.
+withKaTexMathOptions :: Extensions.Extensions -> Options.WriterOptions
+withKaTexMathOptions mathExtensions = Hakyll.defaultHakyllWriterOptions 
+    { 
+        Options.writerExtensions = mathExtensions,
+        Options.writerHTMLMathMethod =  Options.KaTeX ""
+    }
+
+
+-- compiler for posts
+-- captures tags from tag field
+-- uses the pandocCompilerWith instead of regular pandocCompiler to allow you
+-- to add options. writerOptions is defined below, as is the postCtxWithTags
+postCompiler :: Compiler (Item String)
+postCompiler = 
+    let defaultExtensions = Options.writerExtensions defaultHakyllWriterOptions
+        writerOptions = withKaTexMathOptions defaultExtensions
+    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+              >>= saveSnapshot "content"
+              >>= loadAndApplyTemplate "templates/post.html"    postCtx
+              >>= loadAndApplyTemplate "templates/default.html" postCtx
+              >>= relativizeUrls
