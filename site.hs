62320f43051ce01a6b513874907ae4b9b9ce545c
psu <pete.su@gmail.com>
2021-04-28 20:23:37 -0400

    clean

--- site.hs
+++ site.hs
@@ -21,10 +21,6 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "katex/**" $ do
-        route   idRoute
-        compile copyFileCompiler
-
     match (fromList ["about.md", "contact.md"]) $ do
         route $ setExtension "html"
         compile $ pandocCompiler
