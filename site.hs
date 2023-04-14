c62f3742e27e2cfe372b0bf1effb9ebf074c8d5c
psu <psu_13@mac.com>
2023-04-14 15:31:26 -0400

    robots

--- site.hs
+++ site.hs
@@ -17,6 +17,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "robots.txt" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
